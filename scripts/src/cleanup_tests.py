2c1fed988ef5d5f6b23925b1e282b3a4ddd571cf
Connor McG <c.mcg132@gmail.com>
2018-04-09 13:11:58 -0400

    Removed some print statements, fixed chdir
    I'd like to keep the print statements to a minimum, these scripts should will most likely be run without actually seeing the output.

--- scripts/src/cleanup_tests.py
+++ scripts/src/cleanup_tests.py
@@ -1,21 +1,16 @@
 import os, shutil
 
 def removetests():
-    test_count = 0
     bin_count = 0
     for dirpath, dirnames, files in os.walk('.'):
         if '__test__' in dirnames:
-            test_count = test_count + 1
             try:
                 shutil.rmtree(dirpath + "/__test__/bin")
-                bin_count = bin_count + 1
+                bin_count += 1
             except FileNotFoundError:
-                print("This __test__ folder doesn't contain any bin folder")
-    print("Found a total of " + str(test_count) + " __test__ folders.")
-    print("Found " + str(bin_count) + " bin folders within them.")
+                pass
+    print("Removed " + str(bin_count) + " test bin folders.")
 
 if __name__ == "__main__":
-    os.chdir("..")
-    os.chdir("..")
-    os.chdir("./src/file_server")
+    os.chdir("../src/file_server")
     removetests()
