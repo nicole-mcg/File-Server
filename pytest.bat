d9447d3a7d46e94366ffa0def8d2787850fed007
c-mcg <c.mcg132@gmail.com>
2018-04-09 13:38:14 -0400

    Updated command file names to be more clear

--- pytest.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd src
-call python -m pytest
-cd ..
-pause
--- test.bat
+++ test.bat
@@ -1,11 +1,6 @@
 @echo off
-
-cd scripts
-echo Clearing test directories
-call clear_test_dirs
-echo Creating test files
-call create_test_files
+cd src
+echo Testing back end
+call python -m pytest
 cd ..
-
-start "Server" cmd /c run_server.bat
-start "Client" cmd /c run_client.bat
+pause
