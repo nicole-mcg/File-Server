370ccc75b3cd0be2a12db18be09ddddb1765e75c
c-mcg <c.mcg132@gmail.com>
2018-04-14 17:59:02 -0400

    Removed outdated test example

--- src/file_server/web/endpoints/__test__/test_active_clients.py
+++ src/file_server/web/endpoints/__test__/test_active_clients.py
@@ -5,18 +5,6 @@ from file_server.web.endpoints.active_clients import ActiveClientsEndpoint
 
 from file_server.util import create_object
 
-@pytest.mark.skip(reason="This is just a start to the test atm")
+@pytest.mark.skip(reason="Not implemented")
 def test_active_clients():
-
-    def createConnectionObj(client_host, account_name, transferring=False):
-        return create_object({
-            "client_host": client_host,
-            "account": create_object({"name": account_name}),
-            "transferring": transferring,
-        })
-
-    connections = [
-        createConnectionObj("client host", "account name", True)
-    ]
-
-    ActiveClientsEndpoint().handle_request(None, create_object({"connections": connections}), None, None)
+    pass
