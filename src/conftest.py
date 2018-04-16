a59608220dd65e6989893e95941ec59e2220c9ff
c-mcg <c.mcg132@gmail.com>
2018-04-16 14:20:48 -0400

    Added comments

--- src/conftest.py
+++ src/conftest.py
@@ -1,11 +1,15 @@
+# This file is run in the pytest environment before the tests
+
 import pytest
-import tempfile
 import webbrowser
 
+# This method is called before all tests
+# It's used to override any methods
 @pytest.fixture()
 def prepare_for_tests():
 
     def empty_function(url, **kwargs):
         pass
 
+    # Override webbrowser.open so test can't open pages on the internet
     webbrowser.open = empty_function
