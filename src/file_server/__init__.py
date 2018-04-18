7a806c490f94c29b1e7ebfc04481fe95c2825ecf
c-mcg <c.mcg132@gmail.com>
2018-04-18 03:55:41 -0400

    Added environment setup for java wrapper

--- src/file_server/__init__.py
+++ src/file_server/__init__.py
@@ -1,4 +1,4 @@
-import sys
+import sys, builtins, traceback
 from threading import Thread
 
 from file_server.hub.packet_manager import initialize_packet_manager
@@ -15,22 +15,13 @@ def start_hub():
     if isServer:
 
         from .server import FileServer
-        from .web.webserver import create_webserver
+        
 
         # Use FileServer class for hub
         try: 
             hub = FileServer(directory)
         except OSError:
             print("File server could not be started")
-            return
-
-        # Start the webserver
-        try:
-            hub.webserver = create_webserver(hub)
-            Thread(target = hub.webserver.serve_forever).start()
-        except OSError:
-            print("File server could not be started")
-            server.kill()
             return
 
     else:
@@ -56,7 +47,53 @@ def start_hub():
     # Cleanup when the connection has ended
     hub.kill()
 
+
+def installThreadExcepthook():
+    """
+    Workaround for sys.excepthook thread bug
+    From
+http://spyced.blogspot.com/2007/06/workaround-for-sysexcepthook-bug.html
+   
+(https://sourceforge.net/tracker/?func=detail&atid=105470&aid=1230540&group_id=5470).
+    Call once from __main__ before creating any threads.
+    If using psyco, call psyco.cannotcompile(threading.Thread.run)
+    since this replaces a new-style class method.
+    """
+    init_old = Thread.__init__
+    def init(self, *args, **kwargs):
+        init_old(self, *args, **kwargs)
+        run_old = self.run
+        def run_with_except_hook(*args, **kw):
+            try:
+                run_old(*args, **kw)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                sys.excepthook(*sys.exc_info())
+        self.run = run_with_except_hook
+    Thread.__init__ = init
+
+def exception_hook(*args):
+    sys.stdout.write(traceback.print_last() + "\n")
+    sys.stdout.flush()
+    sys.exit(1)
+    builtins.print("test")
+
+sys.excepthook = exception_hook
+
 if __name__ == "__main__":
+
+    installThreadExcepthook()
+
+    builtins.old_print = builtins.print
+
+    def new_print(*objects, sep='', end='\n', file=sys.stdout, flush=False):
+        if file is sys.stdout or file is sys.stderr:
+            builtins.old_print(*objects, sep=sep, end=end, file=sys.stdout, flush=True)
+        else:
+            builtins.old_print(*objects, sep=sep, end=end, file=file, flush=flush)
+
+    builtins.print = new_print
 
     # Make sure args length is correct
     if (len(sys.argv) <= 1):
